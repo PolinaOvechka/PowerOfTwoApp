de671f9421267612f8aba658e5163a9797b4db31
Полина Ярославцева <polinaovechka@gmail.com>
2026-03-06 02:32:41 +0300

    Создание метода последовательного деления

---
+++ Models/DivisionAlgorithm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PowerOfTwoApp.Models
+{
+    // Метод последовательного деления
+    public class DivisionAlgorithm
+    {
+        // Счетчик выполненных операций
+        public int OperationCount { get; private set; }
+
+        public bool Check(long number)
+        {
+            OperationCount = 0;
+            
+            OperationCount++;
+            if (number < 1) ;
+            {
+                return false;
+            }
+
+            while (number%2==0)
+            {
+                OperationCount += 2;
+                number = number/2;
+            }
+
+            OperationCount++;
+            return number == 1;
+        }
+    }
+}
